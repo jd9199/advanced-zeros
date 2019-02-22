72984abbca4fa5f9a31bcb9e9f4aa20f04641245
jd9199 <grafclown999@gmail.com>
2019-02-22 17:56:39 +0300

    Fix index.js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,20 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
-let num_nulls = 1000000000;
-for( let i = 2; i <= base; i++){
-  if (base % i === 0){
-    let count_k1 = 0;
-    while (base % i === 0){
-      count_k1++;
-      base = base / i;
+  let num_nulls = 1000000000;
+  for (let i = 2; i <= base; i++) {
+    if (base % i === 0) {
+      let count_k1 = 0;
+      while (base % i === 0) {
+        count_k1 += 1;
+        base /= i;
+      }
+      let count_k2 = 0;
+      let tmp = number;
+      while (tmp / i > 0) {
+        count_k2 = Math.floor(count_k2 + tmp / i);
+        tmp /= i;
+      }
+      num_nulls = Math.min(num_nulls, Math.floor(count_k2 / count_k1));
     }
-    let count_k2 = 0;
-    let tmp = number;
-    while(tmp / i > 0){
-      count_k2 = Math.floor(count_k2 + tmp / i);
-      tmp = tmp / i;
-    }  
-   num_nulls = Math.min(num_nulls, Math.floor(count_k2 / count_k1));
   }
-}
-return num_nulls;
+  return num_nulls;
 }
